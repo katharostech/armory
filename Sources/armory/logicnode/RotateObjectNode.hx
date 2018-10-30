ab135cf1058fbdd1d149c30c658d290bb71acafd
Zicklag <zicklag@katharostech.com>
2018-10-30 18:41:01 -0500

    Add World Space Option To Rotate Object Node
    Now there is a dropdown for the Rotate Object node that lets you select
    "Local Space" or "World Space".

--- Sources/armory/logicnode/RotateObjectNode.hx
+++ Sources/armory/logicnode/RotateObjectNode.hx
@@ -8,6 +8,7 @@ import armory.trait.physics.RigidBody;
 
 class RotateObjectNode extends LogicNode {
 
+	public var property0:String; // Local Space or World Space
 	var q = new Quat();
 
 	public function new(tree:LogicTree) {
@@ -22,7 +23,17 @@ class RotateObjectNode extends LogicNode {
 
 		q.fromEuler(vec.x, vec.y, vec.z);
 
-		object.transform.rot.mult(q);
+		switch (property0) {
+		case "Local Space":
+			object.transform.rot.mult(q);
+		case "World Space":
+			var ot = object.transform;
+			var prevLoc = ot.loc;
+			ot.loc = new Vec4(0, 0, 0);
+			ot.multMatrix(Mat4.identity().fromQuat(q));
+			ot.loc = prevLoc;
+		}
+
 		object.transform.buildMatrix();
 
 		#if arm_physics
--- blender/arm/logicnode/action_rotate_object.py
+++ blender/arm/logicnode/action_rotate_object.py
@@ -9,10 +9,20 @@ class RotateObjectNode(Node, ArmLogicTreeNode):
     bl_label = 'Rotate Object'
     bl_icon = 'GAME'
 
+    property0 = EnumProperty(
+        items = [
+            ('Local Space', 'Local Space', 'Local Space'),
+            ('World Space', 'World Space', 'World Space'),
+        ],
+        name='', default='Local Space')
+
     def init(self, context):
         self.inputs.new('ArmNodeSocketAction', 'In')
         self.inputs.new('ArmNodeSocketObject', 'Object')
         self.inputs.new('NodeSocketVector', 'Vector')
         self.outputs.new('ArmNodeSocketAction', 'Out')
+
+    def draw_buttons(self, context, layout):
+        layout.prop(self, 'property0')
 
 add_node(RotateObjectNode, category='Action')
